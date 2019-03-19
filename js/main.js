4f6d5df53b976134801b2d8a5077e5db32e9e981
mixonich <mixxxonich@gmail.com>
2019-03-19 20:09:42 +0200

    upd html tags

--- js/main.js
+++ js/main.js
@@ -1,2 +1,1 @@
 "use strict";$(window).load(function(){$(".loader").delay(2600).fadeOut()});
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOlsiJCIsIndpbmRvdyIsImxvYWQiLCJkZWxheSIsImZhZGVPdXQiXSwibWFwcGluZ3MiOiJBQUFBLGFBRUFBLEVBQUVDLFFBQVFDLEtBQUssV0FDYkYsRUFBRSxXQUFXRyxNQUFNLE1BQU1DIiwiZmlsZSI6Im1haW4uanMiLCJzb3VyY2VzQ29udGVudCI6WyJcInVzZSBzdHJpY3RcIjtcblxuJCh3aW5kb3cpLmxvYWQoZnVuY3Rpb24gKCkge1xuICAkKFwiLmxvYWRlclwiKS5kZWxheSgyNjAwKS5mYWRlT3V0KCk7XG59KTsiXX0=
